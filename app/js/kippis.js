2f13e63ddae8b0f4308409a1536c0aa2b9148cd2
Juha-Pekka Rajaniemi <juha.rajaniemi@gmail.com>
2013-06-01 23:38:14 +0300

    Firefox OS project file and work on graph drawing

--- app/js/kippis.js
+++ app/js/kippis.js
@@ -1,5 +1,7 @@
 var kippis = null;
 
+var startDate = new Date(2013,5,29,19,40,0,0);
+
 var drinkhistory = [
     {
         time: new Date(2013, 5, 29, 20, 0, 0, 0),
@@ -7,23 +9,23 @@ var drinkhistory = [
         amount: 33
     },
     {
-        time: new Date(2013, 5, 29, 20, 10, 0, 0),
+        time: new Date(2013, 5, 29, 20, 15, 0, 0),
         percent: 14.3,
         amount: 25
     },
     {
-        time: new Date(2013, 5, 29, 20, 24, 0, 0),
-        percent: 4.6,
+        time: new Date(2013, 5, 29, 20, 35, 0, 0),
+        percent: 4.7,
         amount: 50
     },
     {
-        time: new Date(2013, 5, 29, 20, 30, 0, 0),
+        time: new Date(2013, 5, 29, 21, 30, 0, 0),
         percent: 40,
         amount: 4
     },
     {
-        time: new Date(2013, 5, 29, 21, 0, 0, 0),
-        percent: 5.4,
+        time: new Date(2013, 5, 29, 21, 35, 0, 0),
+        percent: 4.7,
         amount: 33
     }
 ];
@@ -38,21 +40,13 @@ var drinkhistory = [
         graph: null,
         ch: 300,
         cw: 400,
-        personweight: 80,
+        personweight: 88,
         ismale: true,
-        malefactor: 0.75,
-        femalefactor: 0.66,
-        margin: 10
+        malefactor: 0.58,
+        femalefactor: 0.49,
+        margin: 10,
+        alcoholdensity: 0.789
     };
-    
-    /**
-     * mg's of alcohol per minute
-     * 
-     * @type Array
-     */
-    var drinktable = [
-        0,0,0,0,0,5,5,5,0,0,5,0,15,0,0,16.5
-    ];
     
     /**
      * Public methods to use in kippis variable.
@@ -112,58 +106,79 @@ var drinkhistory = [
             $(".weight.slider").find(".btnmore").on('click', function(event)  {
                 event.preventDefault();
                 var elem = $(this).parent().find('.amount');
-                var amount = parseFloat(elem.html());
+                var amount = parseInt(elem.html());
                 amount += 1;
+                vars.personweight = amount;
                 elem.html(amount + "kg");
             });
             $(".weight.slider").find(".btnless").on('click', function(event)  {
                 event.preventDefault();
                 var elem = $(this).parent().find('.amount');
-                var amount = parseFloat(elem.html());
+                var amount = parseInt(elem.html());
                 amount -= 1;
-                if (amount < 0) {
-                    amount = 0;
+                if (amount < 30) {
+                    amount = 30;
                 }
+                vars.personweight = amount;
                 elem.html(amount + "kg");
             });
             
             $(".gender span").on('click', function(event) {
                 $(this).parent().find("span").removeClass("selected");
                 $(this).addClass("selected");
+                if ($(this).hasClass("male")) {
+                    vars.ismale = true;
+                } else {
+                    vars.ismale = false;
+                }
             });
             
             $("button.add").on('click', function(event) {
-                var cl = parseInt($(".servingsize.slider .amount").html()) * 10;
-                var percent = parseFloat($(".alcoholcontent.slider .amount").html()) / 100;
-                drinktable.push(cl * percent);
+                var cl = parseInt($(".servingsize.slider .amount").html());
+                var percent = parseFloat($(".alcoholcontent.slider .amount").html());
+                var n = {
+                    time: new Date(2013, 5, 29, 23, 00, 0, 0),
+                    percent: percent,
+                    amount: cl
+                };
+                drinkhistory.push(n);
                 public.drawGraph();
             });
         },
                 
         drawGraph: function() {
-            drawGraph(alctable(drinktable));
+            //drawGraph(alctable(drinktable));
+            var points = alctable();
+            drawGraph(points);
         },
         
         
     };
     
-    var drawGraph = function(table) {
+    var drawGraph = function(points) {
         var g = vars.graph;
-        g.clearRect(0,0,vars.cw,vars.ch);
+        g.clearRect(0,0,1000,1000);
         //crisp lines
         g.translate(0.5, 0.5);
         g.strokeStyle = "black";
         
-        g.moveTo(0,vars.ch - vars.margin);
-        for (var i = 0; i < table.length; i++) {
-            g.lineTo(2 + i * 2, vars.ch - vars.margin - table[i] * 100 * (vars.ch - (2* vars.margin)));
+        var bottomy = vars.ch - vars.margin;
+                
+        g.moveTo(0,bottomy);
+        g.lineTo(points[0].startx, bottomy);
+
+        for (var i = 0; i < points.length; i++) {
+            g.quadraticCurveTo(points[i].startx, bottomy - points[i].peaky * 200, points[i].peakx, bottomy - points[i].peaky * 200);
+            //g.lineTo(points[i].peakx, bottomy - points[i].peaky * 200);
+            if (points[i].endx > points[i].peakx) {
+                g.lineTo(points[i].endx, bottomy - points[i].endy * 200);
+            }
         }
         g.stroke();
     };
     
-    function alctable(purealcoholperminute) {
-        var burnrateperminute = 0.015 * (1/60);
-        var alcinblood = [];
+    function alctable() {
+        var burnrateperminute = 0.017 * (1/60) * 10;
         
         var waterinbody = vars.personweight;
         if (vars.ismale) {
@@ -172,47 +187,72 @@ var drinkhistory = [
             waterinbody *= vars.femalefactor;
         }
         
+        /**
+         * Wirdmarks formula
+         * 
+         * @param {Number} purealcohol alcohol in milligrams
+         * @returns {Number} Amount of alcohol in blood in promilllse
+         */
         var alcincrease = function(purealcohol) {
-            //milliters of pure alcohol * blood factor * 10 (??) / milliliters of blood in person 
-            return purealcohol * 1.055 * 10 / (waterinbody * 1000);
+            var stantarddrinks = purealcohol / 10 * 1.2;
+            //human body has 80.6% of water in body
+            return ((0.806 * stantarddrinks) / waterinbody) * 10;
         };
         
-        for (var i = 0; i < purealcoholperminute.length; i++) {
-            //if no alc in blood and new alc consumed
-            if (i > 0 && purealcoholperminute[i] == 0 && alcinblood[i-1] == 0) {
-                alcinblood.push(0);
-                continue;
-            //if no alc consumed at 0 mins
-            } else if (i == 0 && purealcoholperminute[i] == 0) {
-                alcinblood.push(0);
-                continue;
-            }
-            
-            if (i > 0) {
-                var newalc = alcinblood[i-1] + alcincrease(purealcoholperminute[i]);
+        //process points of interest
+        var startminutes = startDate.getTime() / 1000 / 60;
+        var poi = [];
+        for (var i = 0; i < drinkhistory.length; i++) {
+            var minutesfromstart = drinkhistory[i].time.getTime() / 1000 / 60 - startminutes;
+            var purealcohol = (drinkhistory[i].percent / 100) * drinkhistory[i].amount * 10 * vars.alcoholdensity;
+            var d = {
+                time: minutesfromstart,
+                alc: purealcohol,
+                promilles: alcincrease(purealcohol)
+            };
+          
+            poi.push(d);
+        }
+        
+        var alcinlastpoint = 0;
+        /**
+         * iterate created poi's and create graph points. Create high point
+         * 30 mins after consuming or less if another one is consumed
+         * 
+         * x = minutes
+         * y = promilles
+         */
+        for (var i = 0; i < poi.length; i++) {
+            var current = poi[i];
+            var difference = 0;
+            //calc difference to ending point
+            if (i == poi.length - 1) {
+                difference = Math.ceil((alcinlastpoint + current.promilles ) / burnrateperminute);
             } else {
-                var newalc = alcincrease(purealcoholperminute[i]);
+                difference = poi[i+1].time - current.time;
             }
             
-            var newval = newalc - burnrateperminute;
-            if (newval < 0) {
-                newval = 0;
+            current.starty = alcinlastpoint;
+            current.startx = current.time;
+            current.endy = alcinlastpoint + current.promilles - (difference * burnrateperminute);
+            if (current.endy < 0) {
+                current.endy = 0;
             }
-            alcinblood[i] = newval;
+            current.endx = current.time + difference;
+            if (difference > 30) {    
+                current.peaky = alcinlastpoint + current.promilles - (30 * burnrateperminute);
+                current.peakx = current.time + 30;
+            } else {
+                current.peaky = alcinlastpoint + current.promilles - (difference * burnrateperminute);
+                current.peakx = current.time + difference;
+            }
+            
+            alcinlastpoint = current.endy;
         }
         
-        if (alcinblood[alcinblood.length-1] > 0) {
-            while (true) {
-                var nextminute = alcinblood[alcinblood.length-1] - burnrateperminute;
-                if (nextminute < 0) {
-                    break;
-                }
-                alcinblood.push(nextminute);
-            }
-        }
-        
-        return alcinblood;
+        return poi;
     }
+    
     
     
     
